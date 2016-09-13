eaeb189bf2300f3a77f9568a87dde97b49677828
Stephen Thoma <sthoma4@gmail.com>
2016-09-13 08:08:36 -0600

    Add environment setup service.

---
+++ services.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+  "os"
+  "fmt"
+  "bufio"
+  "strings"
+)
+
+func Readln(r *bufio.Reader) (string, error) {
+  var (isPrefix bool = true
+       err error = nil
+       line, ln []byte
+      )
+  for isPrefix && err == nil {
+      line, isPrefix, err = r.ReadLine()
+      ln = append(ln, line...)
+  }
+  return string(ln),err
+}
+
+func setupEnvironment() {
+  f, err := os.Open(".env")
+  if err != nil {
+      panic(err)
+  }
+  reader := bufio.NewReader(f)
+  s, e := Readln(reader)
+  for e == nil {
+    pair := strings.Split(s, "=")
+    os.Setenv(pair[0], pair[1])
+
+    s,e = Readln(reader)
+  }
+}
+
+func main() {
+  setupEnvironment()
+}
