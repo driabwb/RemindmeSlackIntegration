af375f07301e0596c9809915d65806d800b2f1f0
driabwb <david.baird@colorado.edu>
2016-09-14 13:43:19 -0600

    Adding a function to retrieve the next reminder to come due.

--- databaseFunctions.go
+++ databaseFunctions.go
@@ -5,7 +5,7 @@ import (
   "log"
   "sync"
   "encoding/json"
-  //"time"
+  "time"
 
   "github.com/gocql/gocql"
 );
@@ -38,9 +38,8 @@ func (cdb *CassandraDB) Close() {
 func (cdb *CassandraDB) InsertRequest(info *Reminder) error{
   date := info.reminderTime.YearDay()
   context, err := json.Marshal(info.context)
-  log.Println(context)
   if nil != err {
-    log.Println("Json Marshal")
+    log.Println("Insert Request Json Marshal Error")
     log.Println(err)
     return err
   }
@@ -49,7 +48,35 @@ func (cdb *CassandraDB) InsertRequest(info *Reminder) error{
     info.reminderTime, context, date, info.user_name,
   ).Exec()
   if nil != err {
+    log.Println("Insert Request Error:")
     log.Println(err)
   }
   return err
 }
+
+func (cdb *CassandraDB) ReadNextRequest() (*Reminder, error){
+  reminder := new(Reminder)
+  var messages []byte
+  err := cdb.session.Query(
+    `SELECT alertTime, messages, user_name FROM Messages WHERE date = ?`,
+    time.Now().YearDay(),
+  ).Consistency(gocql.One).Scan(
+                                &reminder.reminderTime,
+                                &messages,
+                                &reminder.user_name,
+                               )
+  if nil != err {
+    log.Println("Read Request Error")
+    log.Println(err)
+    return nil, err
+  }
+  err = json.Unmarshal(messages, &reminder.context)
+  if nil != err {
+    log.Println("Read Request JSON Unmarshal Error")
+    log.Println(err)
+    return nil, err
+  }
+  return reminder, err
+}
+
+
