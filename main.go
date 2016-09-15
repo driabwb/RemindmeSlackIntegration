fae67db85e947d1c1e03d95863ec0e5bccf0086b
Stephen Thoma <sthoma4@gmail.com>
2016-09-15 10:48:12 -0600

    Move slack api calls into own file.

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
- "encoding/json"
   "net/http"
   "log"
   "os"
@@ -10,46 +9,11 @@ import (
 )
 
 var TOKEN string
-const APIBASEURL string = "https://slack.com/api/"
-var APIENDPOINTS = map[string]map[string]string{
-  "history": {"endpoint": "channels.history", "param": "channel"},
-  "user": {"endpoint": "users.info", "param": "user"},
-}
-
-type HistoryAPIResponse struct{
-  Messages []Message
-}
-
-type Message struct{
-  Text string
-  User string
-  Ts string
-}
 
 type Reminder struct{
   Context []Message
   ReminderTime time.Time
   User string
-}
-
-func getJson(url string, target interface{}) error {
-  r, err := http.Get(url)
-  if err != nil {
-      return err
-  }
-  defer r.Body.Close()
-
-  return json.NewDecoder(r.Body).Decode(target)
-}
-
-func makeAPICall(endpoint map[string]string, param map[string]string) *HistoryAPIResponse {
-  result := new(HistoryAPIResponse)
-  url := APIBASEURL + endpoint["endpoint"] + "?token=" + TOKEN
-  if endpoint_param, ok := endpoint["param"]; ok {
-    url += "&" + endpoint_param + "=" + param[endpoint_param]
-  }
-  getJson(url, result)
-  return result
 }
 
 func handleNewRequest(w http.ResponseWriter, r *http.Request){
@@ -59,13 +23,17 @@ func handleNewRequest(w http.ResponseWriter, r *http.Request){
     w.Write([]byte("Error: POST only"))
     return
   }
- r.ParseForm()
- token := r.Form["token"]
- channel_id := r.Form["channel_id"]
- user_name := r.Form["user_name"]
- log.Print("Token: " + token[0] + "| channel_id: " + channel_id[0] + "| user_name: " + user_name[0])
- makeAPICall(APIENDPOINTS["history"], map[string]string{"channel": strings.Join(channel_id, ",")})
- w.Write([]byte("Your reminder has been set."))
+  r.ParseForm()
+  token := r.Form["token"]
+  channel_id := r.Form["channel_id"]
+  user_name := r.Form["user_name"]
+  log.Print("Token: " + token[0] + "| channel_id: " + channel_id[0] + "| user_name: " + user_name[0])
+  //history, err := getHistory(strings.Join(channel_id, ","))
+  if err != nil {
+    log.Print("Houston has a problem")
+    log.Fatal(err)
+  }
+  w.Write([]byte("Your reminder has been set."))
 }
 
 func handleReminderTrigger(reminder *Reminder){
---
+++ slackService.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+  "encoding/json"
+  "net/http"
+  "log"
+  "io"
+  //"io/ioutil"
+)
+
+const APIBASEURL string = "https://slack.com/api/"
+var APIENDPOINTS = map[string]string {
+  "history": "channels.history",
+  "user": "users.info",
+}
+
+type HistoryAPIResponse struct {
+  Messages []Message
+}
+
+type Message struct {
+  Text string
+  User string
+  Ts string
+}
+
+type UserAPIResponse struct {
+  Id string
+  User string
+  Profile UserAPIResponseProfile
+}
+
+type UserAPIResponseProfile struct {
+  First_name string
+  Last_name string
+}
+
+func getJson(json_to_decode io.Reader, target interface{}) error {
+  return json.NewDecoder(json_to_decode).Decode(&target)
+}
+
+func makeAPICall(endpoint string, params map[string]string, dest interface{}) error {
+  url := APIBASEURL + endpoint + "?token=" + TOKEN
+  if len(params) != 0 {
+    for param, val := range params {
+      url += "&" + param + "=" + val
+    }
+  }
+
+  res, err := http.Get(url)
+  defer res.Body.Close()
+  if err != nil {
+    return err
+  }
+
+  return getJson(res.Body, dest)
+}
+
+func getHistory(channel string) (*HistoryAPIResponse, error) {
+  response := new(HistoryAPIResponse)
+  err := makeAPICall(APIENDPOINTS["history"], map[string]string{"channel": channel, "count": "5"}, response)
+
+  if err != nil {
+    log.Print("Error: HTTP GET failed")
+    log.Print(err)
+    return nil, err
+  }
+
+  return response, nil
+}
+
+func getUser(user_id string) (*UserAPIResponse, error) {
+  response := new(UserAPIResponse)
+  err := makeAPICall(APIENDPOINTS["user"], map[string]string{"user": user_id}, response)
+
+  if err != nil {
+    log.Print("Error: HTTP GET failed")
+    log.Print(err)
+    return nil, err
+  }
+
+  return response, nil
+}
